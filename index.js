b246eb87987d58ff07b7b83e8fed28a74f65e612
Juan Daza <cadillacmedina@live.com>
2019-05-13 15:16:53 -0500

    Uso node Express creacion estructura

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const http = require('http');
 const color = require('colors');
+const express = require('express');
+// Se carga el módulo de HTTP
+const http = require("http");
+
+// Creación del servidor HTTP, y se define la escucha
+// de peticiones en el puerto 8000
+http.createServer(function(request, response) {
+
+   // Se define la cabecera HTTP, con el estado HTTP (OK: 200) y el tipo de contenido
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Se responde, en el cuerpo de la respuesta con el mensaje "Hello World"
+   response.end('Hola Mundo!\n');
+}).listen(8000);
+
+// Se escribe la URL para el acceso al servidor
+console.log('Servidor en la url http://127.0.0.1:8000/');
+
+
+
+
+
+/*
 const handleServer = function(req,res)
 {
     res.writeHead(200,{'content-type':'text/html'})
@@ -13,6 +35,7 @@ server.listen(3000, function(){
     console.log('Server on port 3000'.green);
 
 });
+*/
 
 
 //Método Asincrono
